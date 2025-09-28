05b1fff2262b3246d5275bae85b26512ea5b7ee5
bmunoz-12 <ci710582232023@josepardo.edu.pe>
2025-09-28 01:49:54 -0500

    CineStarV3

--- js/cine.js
+++ js/cine.js
@@ -22,10 +22,16 @@ const getCine = async () => {
 			`
 		});
 
-		//recorido img
-		let imghtml1 = `
-		<img src="img/cine/${cines.id}.2.jpg"/>
-		`
+		//recorrido Pelicula Horario
+		let peliHorarioHtml = ``
+		cines.peliculas.forEach((ph, index) => {
+			peliHorarioHtml +=`
+			<div class="fila ${index % 2 === 0 ? 'impar' : ''}">
+				<div class="celda-titulo">${ph.Titulo}</div>
+				<div class="celda">${ph.Horarios}</div>
+			</div>
+			`
+		});
 
 
         let html = `
@@ -36,13 +42,13 @@ const getCine = async () => {
 						<p>Teléfono: ${cines.Telefonos}</p>
 						<br/>
 						<div class="tabla">
-							${tarifashtml}
+							${tarifashtml} 
 						</div>
 						<div class="aviso">
 							<p>A partir del 1ro de julio de 2016, Cinestar Multicines realizará el cobro de la comisión de S/. 1.00 adicional al tarifario vigente, a los usuarios que compren sus entradas por el aplicativo de Cine Papaya para Cine Star Comas, Excelsior, Las Américas, Benavides, Breña, San Juan, UNI, Aviación, Sur, Porteño, Tumbes y Tacna.</p>
 						</div>
 					</div>
-					${imghtml1}
+					<img src="img/cine/${cines.id}.2.jpg"/>
 					<br/><br/><h4>Los horarios de cada función están sujetos a cambios sin previo aviso.</h4><br/>
 					<div class="cine-info peliculas">
 						<div class="tabla">
@@ -50,27 +56,12 @@ const getCine = async () => {
 								<div class="celda-cabecera">Películas</div>
 								<div class="celda-cabecera">Horarios</div>
 							</div>
-							<div class="fila impar">
-								<div class="celda-titulo">GUERRERO</div>
-								<div class="celda">13:00 / 13:30 / 14:00 / 15:00 / 15:30 / 16:00 / 17:00 / 17:30 / 18:00 / 19:00 / 20:00 / 21:00 / 21:55</div>
-							</div>
-							<div class="fila">
-								<div class="celda-titulo">LA LEYENDA DE LA BELLA DURMIENTE</div>
-								<div class="celda">19:45 / 21:30</div>
-							</div>
-							<div class="fila impar">
-								<div class="celda-titulo">ROGUE ONE</div>
-								<div class="celda">13:00 / 14:00 / 15:30 / 16:30 / 18:00 / 19:00 / 19:30 / 20:30 / 21:30 / 21:55</div>
-							</div>
-							<div class="fila">
-								<div class="celda-titulo">MOANA</div>
-								<div class="celda">13:00 / 15:15 / 17:30</div>
-							</div>
+							${peliHorarioHtml}							
 						</div>
 					</div>
 				</div>
 				<div>
-					<img style="float:left;" src="img/cine/1.3.jpg" alt="Imagen del cine"/>
+					<img style="float:left;" src="img/cine/${cines.id}.3.jpg" alt="Imagen del cine"/>
 					<span class="tx_gris">Precios de los juegos: desde S/1.00 en todos los Cine Star.<br/>
 						Horario de atención de juegos es de 12:00 m hasta las 10:30 pm. 
 						<br/><br/>
